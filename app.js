6acbb2bad451637e615928a3c3f5e9d2c99a41d5
Yevhenii Kachan <djianosis@gmail.com>
2025-06-23 21:51:43 +0300

    Add headers

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const characterRouters = require("./routers/characters-routers");
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use("/api/characters", characterRouters);
 
